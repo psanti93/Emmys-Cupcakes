342b989ad99c00971ab9cdf9559e890a342a5c86
Paul Santiago <paulsantiago282@gmail.com>
2023-08-12 21:54:22 -0400

    Update cupcake model, loop through list of cupcakes, add cupcakes array

--- src/app/cupcakes/cupcake.model.ts
+++ src/app/cupcakes/cupcake.model.ts
@@ -1,7 +1,7 @@
 export class Cupcake{
-  constructor(public name: string, public desciption: string, public image: string){
+  constructor(public name: string, public description: string, public image: string){
     this.name = name;
-    this.desciption = desciption;
+    this.description = description;
     this.image = image;
 
   }
--- src/app/cupcakes/cupcakes.component.ts
+++ src/app/cupcakes/cupcakes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cupcake } from './cupcake.model';
 
 @Component({
   selector: 'app-cupcakes',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class CupcakesComponent {
 
+  cupcakes = [
+    new Cupcake('Zesty-Lemon', 'Lemony Cupcake Special', "https://preppykitchen.com/wp-content/uploads/2020/03/lemono-cupcakes-recipe.jpg"),
+    new Cupcake('Emmy Special', 'Emmy Red Velvet Special','https://www.lovelesscafe.com/wp-content/uploads/2019/11/red-velvet-cupcake.jpg')
+  ]
 }
